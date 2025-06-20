7192d2d5eb52f4f0acaa77d840970d2ebcd58199
Jon Castro <jonattancast@gmail.com>
2025-06-20 11:17:02 -0400

    added calculator files

---
+++ functions/get_files_info.py
@@ -0,0 +1,9 @@
+import os
+def get_files_info(working_directory, directory=None):
+    if directory not in working_directory:
+        print(f"Error, Cannot list '{directory}' as it is outside the permitted working directory")
+
+    if not os.path.isdir(directory):
+        print(f'Error: "{directory}" is not a directory')
+
+    
